240ad4d3817146e88a190c2723ee5cee59adb1b6
dilanrinconhbtn <937@holbertonschool.com>
2020-05-19 18:03:31 -0500

    finish all proyect maze with blind mode

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,44 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class PlayerController : MonoBehaviour
 {
-
     public Rigidbody rb;
     public float speed = 1000f;
     private int score = 0;
     public int health = 5;
+    public Text scoreText;
+    public Text healthText;
+    public Text WinLoseText;
+    public GameObject WinLoseObject;
+    public static bool GameIsPaused = false;
+
     // Start is called before the first frame update
     void Update()
     {
         if(health == 0)
         {
-            Debug.Log("Game Over!");
-            SceneManager.LoadScene(0);
+            StartCoroutine("LoadScene", 3f);
         }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(1);
+        }
+
     }
+    IEnumerator LoadScene(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(0);
+    }
+
     // Update is called once per frame
-    // fixed beacuse includes physiscs  
+    // fixed beacuse includes physiscs
+    //move awsd
     void FixedUpdate()
     {
         if( Input.GetKey("d"))
@@ -38,23 +58,51 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(0, 0, speed * Time.deltaTime);
         }
     }
+    //Ontrigger destroy and count healt or score or win
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Pickup"))
         {
             Destroy(other.gameObject);
             score++;
-            Debug.Log("Score: " + score.ToString());
+            SetScoreText();
         }
 
         if (other.gameObject.CompareTag("Trap"))
         {
             health--;
-            Debug.Log("Health: " + health.ToString());
+            SetHealthText();
+            if (health == 0)
+            {
+                //change color and text to text
+                WinLoseText.text = "Game Over!";
+                WinLoseText.color = Color.white;
+                //change color to image
+                WinLoseObject.GetComponent<UnityEngine.UI.Image>().color = Color.red;
+                //Activate the image to show if Lose
+                WinLoseObject.SetActive(true);
+                
+            }
         }
         if (other.gameObject.CompareTag("Goal"))
         {
-            Debug.Log("You win!");
+            //change color and text to text
+            WinLoseText.text = "You Win!";
+            WinLoseText.color = Color.black;
+            //change color to image
+            WinLoseObject.GetComponent<UnityEngine.UI.Image>().color = Color.green;
+            //Activate the image to show if win
+            WinLoseObject.SetActive(true);
+            StartCoroutine("LoadScene", 3f);
         }
     }
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+    void SetHealthText()
+    {
+        healthText.text = "Health: " + health.ToString();
+    }
+
 }
